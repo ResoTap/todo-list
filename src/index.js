b29488c5a17c6bc7bb39ab4214d65f5405197771
Patrick Oser <patrick.oser148@gmail.com>
2023-08-19 10:46:12 -0400

    Add style to 'projectCon'.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import {
   makeHeader,
   makeProjectContent,
   makeFooter,
+  makeNewProject,
 } from "./modules/projects-view";
 import "./style.css";
 import "normalize.css";
 
 makeHeader();
 makeProjectContent();
+makeNewProject();
 makeFooter();
 
 console.log("hola");
